5b1a6af9d022fd03932364ff30dba6908829fb0c
Braden Simpson <braden@uvic.ca>
2012-06-26 16:35:12 -0700

    added generator

---
+++ src/generator/Generator.java
@@ -0,0 +1,16 @@
+package generator;
+
+/**
+ * <code>Generator</code will go through each commit
+ * and create relationships
+ * @author braden
+ *
+ */
+public class Generator
+{
+	public Soc
+	public Generator() { }
+	
+	
+	
+}
