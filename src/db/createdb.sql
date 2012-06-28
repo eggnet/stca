359a043d531017714d4917c88b596dc35e3dd4cf
infiro <inferno71123@yahoo.com>
2012-06-28 11:38:50 -0700

    added createdb.sql

--- src/db/createdb.sql
+++ src/db/createdb.sql
@@ -1,1 +1,31 @@
---//Todo
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+--///////////////// PATTERNS TABLE /////////////////--
+CREATE SEQUENCE patterns_id_seq
+	START WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+	
+CREATE TABLE patterns ( 
+	pattern_id integer NOT NULL PRIMARY KEY DEFAULT NEXTVAL('patterns_id_seq'::regclass),
+	person1 varchar(255),
+	person2 varchar(255),
+	type varchar(255),
+	exist integer,
+	not_exist integer,
+	passed varchar(10)
+);
+
+ALTER SEQUENCE patterns_id_seq OWNED BY patterns.pattern_id;
